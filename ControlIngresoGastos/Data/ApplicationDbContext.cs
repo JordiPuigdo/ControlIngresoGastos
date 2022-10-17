6adaad5b475a3b4823f258b02969bd8c636a1d18
jpuigdollers <jpuigdollers@PC-JPUIGDOLLERS>
2022-10-17 18:50:21 +0200

    Add project files.

---
+++ ControlIngresoGastos/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using ControlIngresoGastos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControlIngresoGastos.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("AuthConnectionString");
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
---
+++ ControlIngresoGastos/Models/Categoria.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ControlIngresoGastos.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(120)]
+        [Display(Name ="Nombre Categoria")]
+        public String NombreCategoria { get; set; }
+        [Required]
+        [MaxLength(120)]
+        [Display(Name = "Tipo")]
+        public String Tipo{ get; set; } //IN Ingreso GA Gasto
+
+        [Required]
+        public bool Estado { get; set; }
+
+    }
+}
